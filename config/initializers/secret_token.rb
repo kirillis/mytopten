9959fee05a9a2ae704d3b7b00ed08c24fd6e35e0
Kris Immig <kris.immig@gmail.com>
2015-06-20 15:39:30 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mytopten::Application.config.secret_key_base = '02d4368bf82ebdfd3ed6a6b3b89030d3015666f66605bddbf48a8fda1423e03d9ca517136ec274defa81d05233cba6dffbd2e847b20f9a1aaada5efbcd63a7d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mytopten::Application.config.session_store :cookie_store, key: '_mytopten_session'
