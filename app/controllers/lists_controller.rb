a9e7b4673b6ccb6bf8b59b0b923735e2eeed4921
Kris Immig <kris.immig@gmail.com>
2015-06-20 23:07:42 +0200

    Add create new list form to root page.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,7 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
+    @new_list = List.new
     respond_to do |format|
       format.html
       format.json { render json: @lists }
@@ -9,5 +10,17 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @new_list_item = @list.build_listitem
   end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to action: :index
+  end
+
+  private
+    def list_params
+      params.require(:list).permit(:title, :description)
+    end
 end
