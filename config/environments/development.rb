f5118abd9b758e9c3b70f274b9ca9b763f17ea6d
Kris Immig <kris.immig@gmail.com>
2015-09-06 13:41:51 +0200

    Add react configuration to environments.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Mytopten::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # React config
+  config.react.variant = :development
 end
