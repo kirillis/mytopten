c5feef9c1d0684964c5a19e3f80de397ac202d60
Kris Immig <kris.immig@gmail.com>
2015-09-06 15:17:02 +0200

    update db schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620185335) do
+ActiveRecord::Schema.define(version: 20150906125159) do
 
   create_table "list_items", force: true do |t|
     t.string   "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20150620185335) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "list_id"
+    t.string   "image_url"
+    t.string   "link"
   end
 
   create_table "lists", force: true do |t|
