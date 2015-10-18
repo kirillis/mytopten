2e1a56d00de0bab817c6e6798c3a97a8088f2fdf
Kris Immig <kris.immig@gmail.com>
2015-10-18 13:33:49 +0200

    Add sessions.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if login(params[:name], params[:password])
+      flash[:success] = 'Welcome back!'
+      redirect_back_or_to root_path
+    else
+      flash.now[:warning] = 'E-mail and/or password is incorrect.'
+      render 'new'
+    end
+  end
+
+  def destroy
+    logout
+    flash[:success] = 'See you!'
+    redirect_to user_log_in_path
+  end
+end
