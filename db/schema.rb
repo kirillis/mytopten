0836b036413bf5672ca1caa89792bb40626046b1
Kris Immig <kris.immig@gmail.com>
2015-10-19 12:50:21 +0200

    Updtae schema and seeds files.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018100802) do
+ActiveRecord::Schema.define(version: 20151018123339) do
 
   create_table "list_items", force: true do |t|
     t.string   "title"
@@ -29,7 +29,10 @@ ActiveRecord::Schema.define(version: 20151018100802) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "lists", ["user_id"], name: "index_lists_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
