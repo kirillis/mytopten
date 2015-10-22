b45b206761f96162167f291f05f2366a239e0279
Kris Immig <kris.immig@gmail.com>
2015-10-22 14:30:38 +0200

    Move actions into namespaced obejct.

--- app/assets/javascripts/flux/actions.js
+++ app/assets/javascripts/flux/actions.js
@@ -1,32 +1,71 @@
 App.actions = {
-  updateItem: function(itemData, newItemData) {
-    var payload = {
-      itemData: itemData,
-      newItemData: newItemData
-    };
-    this.dispatch(App.constants.ITEM_UPDATE, payload);
-    App.updateItem(payload, this);
-  },
+  listItem: {
+    update: function(itemData, newItemData) {
+      var payload = {
+        itemData: itemData,
+        newItemData: newItemData
+      };
 
-  addNewItem: function(newItemData) {
-    var listID = this.flux.store("ListStore").getID();
-    var listItemID = App.makeId();
-    var itemData = {
-      title: newItemData.title,
-      link: newItemData.amazon_url,
-      image_url: newItemData.thumbnail_url,
-      list_id: listID,
-      rank: 0,
-      listItemID: listItemID
-    };
+      this.dispatch(App.constants.ITEM_UPDATE, payload);
+      App.updateItem(
+        payload,
+        function(data) {
+          this.dispatch(App.constants.ITEM_UPDATE_SUCCESS, data);
+        }.bind(this),
+        function(payload) {
+          this.dispatch(App.constants.ITEM_UPDATE_FAILURE, payload);
+        }.bind(this)
+      );
+    },
 
-    // Optimistically add item to UI.
-    this.dispatch(App.constants.ITEM_ADD, itemData);
-    App.saveItem(itemData, this);
-  },
+    add: function(newItemData) {
+      var listID = this.flux.store("ListStore").getID();
+      var listItemID = App.makeId();
+      var itemData = {
+        title: newItemData.title,
+        link: newItemData.amazon_url,
+        image_url: newItemData.thumbnail_url,
+        list_id: listID,
+        rank: 0,
+        listItemID: listItemID
+      };
 
-  deleteItem: function(itemId) {
-    // Optimistically add item to UI.
-    this.dispatch(App.constants.ITEM_DELETE, itemId);
+      this.dispatch(App.constants.ITEM_ADD, itemData);
+      App.saveItem(
+        itemData,
+        function(savedItem) {
+          this.dispatch(App.constants.ITEM_ADD_SUCCESS, savedItem);
+        }.bind(this),
+        function(error) {
+          this.dispatch(App.constants.ITEM_ADD_FAILURE, error);
+        }.bind(this)
+      );
+    },
+
+    delete: function(itemId) {
+      App.deleteItem(
+        itemId,
+        function(itemId) {
+          this.dispatch(App.constants.ITEM_DELETE, itemId);
+        }.bind(this)
+      );
+    }
   },
+  list: {
+    update: function(listData) {
+      this.dispatch(App.constants.LIST_UPDATE, listData.newData);
+
+      listData.newData.listId = this.flux.store("ListStore").getID();
+      App.updateList(
+        listData.newData,
+        function(data) {
+          this.dispatch(App.constants.LIST_UPDATE_SUCCESS, data);
+        }.bind(this),
+        function(error) {
+          listData.error = error;
+          this.dispatch(App.constants.LIST_UPDATE_FAILURE, listData);
+        }.bind(this)
+      );
+    },
+  }
 };
