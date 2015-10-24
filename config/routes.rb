0d577cc55099e766103cbbb230230e3e1d9db867
Kris Immig <kris.immig@gmail.com>
2015-10-24 17:47:06 +0200

    Add tags views and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,28 @@ Mytopten::Application.routes.draw do
   # ROOT
   root 'home#show'
 
+  # TAGS
+  get 'tags', to: 'tags#index', as: 'tags'
+  get 'tags/:tag_name', to: 'tags#show', as: 'tag'
+
   # USERS
   get 'register' => 'users#new', as: :user_register
-  get '/login', to: 'sessions#new', as: :user_log_in
-  get '/:name', to: 'users#show', as: 'user'
+  get 'login', to: 'sessions#new', as: :user_log_in
+  get ':name', to: 'users#show', as: 'user'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  delete '/logout', to: 'sessions#destroy', as: :user_log_out
+  delete 'logout', to: 'sessions#destroy', as: :user_log_out
 
   # LISTS
-  get '/:user_name/lists', to: 'lists#index', as: 'user_lists'
-  get '/:user_name/:list_id', to: 'lists#show', as: 'user_list'
-  get '/:user_name/:list_id/edit', to: 'lists#edit', as: 'user_list_edit'
-  get '/:user_name/lists/new', to: 'lists#new', as: 'user_list_new'
+  get ':user_name/lists', to: 'lists#index', as: 'user_lists'
+  get ':user_name/:list_id', to: 'lists#show', as: 'user_list'
+  get ':user_name/:list_id/edit', to: 'lists#edit', as: 'user_list_edit'
+  get ':user_name/lists/new', to: 'lists#new', as: 'user_list_new'
   resources :lists, only: [:index, :create, :update, :destroy]
   resources :list_items, only: [:create, :update, :destroy, :edit]
 
   # SEARCH
-  get 'search/amazon/' => 'search#amazon', as: :search_amazon
-  get 'search/' => 'search#index'
+  get 'search/amazon' => 'search#amazon', as: :search_amazon
+  get 'search' => 'search#index'
 
 end
