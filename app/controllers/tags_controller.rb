3f48c01db3d435a64da3fcf6c959960276358643
Kris Immig <kris.immig@gmail.com>
2015-11-08 16:15:38 +0100

    Add search method to tag controller.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,8 +3,18 @@ class TagsController < ApplicationController
     @tags = ActsAsTaggableOn::Tag.all
   end
 
+  def search
+    @tags = ActsAsTaggableOn::Tag.where("name like ?", "%#{params[:tag_query]}%")
+    respond_to do |format|
+      format.html
+      format.json {
+        render json: @tags, root: false
+      }
+  end
+
   def show
     @tag_name = params[:tag_name]
     @lists = List.tagged_with(@tag_name)
+    end
   end
 end
