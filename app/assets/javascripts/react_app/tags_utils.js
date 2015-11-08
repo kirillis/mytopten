c1b1010981198c90a1dd72aa38041456482ad7b2
Kris Immig <kris.immig@gmail.com>
2015-11-08 16:20:01 +0100

    Add tag utils functions.

---
+++ app/assets/javascripts/react_app/tags_utils.js
@@ -0,0 +1,38 @@
+App.updateTags = function(payload, success, error) {
+  // console.log('updateTags()', payload);
+  $.ajax({
+    url: '/lists/' + payload.listId + '/tags',
+    method: 'PUT',
+    data: JSON.stringify(payload),
+    dataType: 'json',
+    contentType: 'application/json',
+
+    success: function(data, textStatus, jqXHR) {
+      success(data);
+    },
+
+    error: function(jqXHR, textStatus, errorThrown) {
+      alert('Error updating list: ' + errorThrown);
+      error(errorThrown);
+    },
+  });
+};
+
+App.removeTag = function(payload, success, error) {
+  $.ajax({
+    url: '/lists/' + payload.listId + '/tag',
+    method: 'DELETE',
+    data: JSON.stringify(payload),
+    dataType: 'json',
+    contentType: 'application/json',
+
+    success: function(data, textStatus, jqXHR) {
+      success(data);
+    },
+
+    error: function(jqXHR, textStatus, errorThrown) {
+      alert('Error updating list: ' + errorThrown);
+      error(errorThrown);
+    },
+  });
+};
