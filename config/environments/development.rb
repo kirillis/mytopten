43920d9c984f4b92501739eb4f6abbc91b7d8021
Kris Immig <kris.immig@gmail.com>
2016-06-09 19:53:52 +0200

    Add bullet gem config.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,13 @@ Mytopten::Application.configure do
 
   # React config
   config.react.variant = :development
+
+  # Bullet cofig
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
 end
