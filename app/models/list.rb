c8ea81aef3e0b66dc3eaef0fc3d05389849474d5
Kris Immig <kris.immig@gmail.com>
2017-08-11 16:22:55 +0200

    Add more validations.

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,6 @@ class List < ActiveRecord::Base
   acts_as_votable
   has_many :list_items, dependent: :delete_all
   belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true
 end
