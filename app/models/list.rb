047cea7ee86891c86863365ad26c5cd8192e8abe
Kris Immig <kris.immig@gmail.com>
2017-09-02 22:09:24 +0200

    Add publoshed scope to model.

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,4 +5,6 @@ class List < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true
   validates :description, presence: true
+
+  scope :published, -> { where(public: true) }
 end
