da2630ac25e5c66a865543a7f2e4d9541e6c2863
Kris Immig <kris.immig@gmail.com>
2017-09-02 17:42:53 +0200

    Update toaster css.

--- app/assets/javascripts/toasts.js
+++ app/assets/javascripts/toasts.js
@@ -1,20 +1,10 @@
 toastr.options = {
   "closeButton": true,
-  "debug": false,
-  "newestOnTop": false,
-  "progressBar": false,
   "positionClass": "toast-bottom-right",
-  "preventDuplicates": false,
-  "onclick": null,
-  "showDuration": "3000",
-  "hideDuration": "500",
-  "timeOut": "5000",
-  "extendedTimeOut": "1000",
-  "showEasing": "swing",
-  "hideEasing": "linear",
-  "showMethod": "fadeIn",
-  "hideMethod": "fadeOut"
+  "timeOut": 3000,
+  "extendedTimeOut": 5000,
 }
+
 
 $(function() {
   var messages = $('.flash-messages').data('messages');
