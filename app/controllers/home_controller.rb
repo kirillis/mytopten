3ea8614fc10ac6c3041ae3b7c2f8e98cec5cdd8d
Kris Immig <kris.immig@gmail.com>
2017-09-02 18:25:43 +0200

    Order by votes.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def show
     @tags = ActsAsTaggableOn::Tag.most_used(20)
-    @lists = List.includes(:user, :list_items).limit(50)
+    @lists = List.includes(:user, :list_items, :tags).order(cached_votes_total: :desc).limit(50)
   end
 
 end
