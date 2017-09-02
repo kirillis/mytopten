5c980473132c48c54c4258f80e8eb41766979919
Kris Immig <kris.immig@gmail.com>
2017-09-02 22:11:33 +0200

    Remove unused tags setting.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Mytopten
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     ActsAsTaggableOn::Tagging.belongs_to :taggable, polymorphic: true, touch: true
+    ActsAsTaggableOn.remove_unused_tags = true
 
   end
 end
