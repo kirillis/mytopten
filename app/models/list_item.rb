1e8e0fe90b57f11c72921c445848d814c8dcef2a
Kris Immig <kris.immig@gmail.com>
2017-09-23 18:08:42 +0200

    Remove paperclip methods.

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -3,32 +3,6 @@ class ListItem < ActiveRecord::Base
   default_scope { order(:rank) }
   before_create :calc_rank
 
-  # This method associates the attribute ":image_main" with a file attachment
-  has_attached_file :image_main,
-                    styles: {
-                      thumb: '150x150>',
-                      medium: '200x200#'
-                    },
-                    :convert_options => {
-                      :thumb => '-quality 80 -strip',
-                      :medium => '-quality 80 -strip'
-                    }
-
-  # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :image_main, :content_type => /\Aimage\/.*\Z/
-
-  # Validate filename
-  validates_attachment_file_name :image_main, :matches => [/png\Z/, /jpe?g\Z/]
-
-
-  def image_thumb
-    self.image_thumb_url ? self.image_thumb_url : self.image_main.url(:thumb)
-  end
-
-  def image_large
-    self.image_large_url ? self.image_large_url : self.image_main.url
-  end
-
   def move_to(new_rank)
     if new_rank < rank then
       sql = "UPDATE list_items
