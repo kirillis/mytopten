1d856c2c89bfee9cc96f345988f8a98031e23f99
Kris Immig <kris.immig@gmail.com>
2017-10-07 20:04:42 +0200

    Add alert react component.

---
+++ app/assets/javascripts/components/alert.js.jsx
@@ -0,0 +1,10 @@
+var Alert = React.createClass({
+
+  render: function () {
+        return (
+            <div className="u-bg-blue-light u-c-white u-t-align-center u-py-3 u-px-2 u-fw-100 u-br-3">
+                <span className="u-fw-700">Click a tag</span> to start filtering. You can <span className="u-fw-700">combine multiple</span> tags to find the content you are looking for.
+            </div>
+        )
+    }
+});
