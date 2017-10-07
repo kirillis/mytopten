e35a223349f6b4660ac767cd766b3e5c27d89d00
Kris Immig <kris.immig@gmail.com>
2017-10-07 20:05:26 +0200

    Use new tag and alert comp. Desing 2.

--- app/assets/javascripts/components/tag_filter.js.jsx
+++ app/assets/javascripts/components/tag_filter.js.jsx
@@ -17,19 +17,21 @@ var TagFilter = React.createClass({
         location.search =newSearchUrl;
     },
 
-    handleActiveTagClick(tag) {
-        location.search = App.removeTagFromFilter(tag);
+    handleActiveTagClick(event, tag) {
+        location.search = App.removeTagFromFilter(tag.name);
+    },
+
+    getHeadline() {
+        return(
+            <h2>Lists with tags</h2>
+        )
     },
 
     render: function () {
-        var activeTagsList = this.state.activeTagsArray.map(function(tag, index) {
+        var activeTagsList = this.state.activeTagsArray.map(function(item, index) {
+            let tagData = {name: item};
             return (
-                <span
-                    key={index}
-                    className='Tag u-p-05 u-bg-gamma u-color-white u-mb-05 u-mr-05'
-                    onClick={this.handleActiveTagClick.bind(this, tag)}>
-                    {tag}
-                </span>
+                <Tag type="remove" key={index} data={tagData} tagClickHandler={ this.handleActiveTagClick } />
             )
         }.bind(this));
 
@@ -39,43 +41,31 @@ var TagFilter = React.createClass({
                 this.state.activeTagsArray.indexOf(item.name) === -1 &&
                 item.name.indexOf(this.state.filterValue) !== -1
             ) {
-                return <Tag type="filter" key={item.id} data={item} addFilterHandler={ this.addFilterHandler }/>;
+                return <Tag type="add" key={item.id} data={item} tagClickHandler={ this.addFilterHandler }/>;
             }
             return false;
         }.bind(this));
 
         return (
-            <div>
+            <div className="container u-mt-3">
 
-                <div className="u-bg-beta u-mb-3">
-                    <div className="container u-py-2">
-                        <div className="Grid">
-                            <div className="Grid-cell 2-of-3--desk">
-
-                                <h2>{activeTagsList.length > 0 ? 'Filtering for these tags' : 'Filter all lists by tags.'}</h2>
-                                <div>{activeTagsList.length > 0 ? activeTagsList : 'Click a tag to start filtering.'}</div>
-                                <p className="u-t-muted">{activeTagsList.length === 0 ? '' : 'Click a tag to remove it from the filter.'}</p>
-                            </div>
-                        </div>
-                    </div>
+                <h2>Avilable tags</h2>
+                <div className="Form u-d-inline-block">
+                    <input
+                        onChange={this.handleChange}
+                        id="filter"
+                        type="text"
+                        name="filter"
+                        placeholder="Search..." />
                 </div>
 
-                <div className="container">
-                    <div className="Grid">
-                        <div className="Grid-cell 1-of-4">
-                            <div className="Form">
-                                <label htmlFor="filter">Search all tags for:</label>
-                                <input
-                                    onChange={this.handleChange}
-                                    id="filter"
-                                    type="text"
-                                    name="filter" />
-                            </div>
-                        </div>
-                    </div>
-                    <div>{availableTagsList}</div>
-                    <p className="u-t-muted">Click a tag to add it to the filter.</p>
+                <div>{availableTagsList}</div>
+
+                <div className="u-mt-2">
+                    {activeTagsList.length > 0 ? this.getHeadline() : <Alert />}
+                    <div>{activeTagsList}</div>
                 </div>
+
             </div>
         );
     }
