b24f23101bc1dffaa49e9c7882218d348a2e4677
DESKTOP-UQMNGE8\Kris Immig <kris.immig@gmail.com>
2017-12-17 11:54:07 +0100

    Remove s3 image field migration.

--- db/migrate/20160615184800_add_image_main_to_list_item.rb
+++
@@ -1,9 +0,0 @@
-class AddImageMainToListItem < ActiveRecord::Migration
-  def self.up
-    add_attachment :list_items, :image_main
-  end
-
-  def self.down
-    remove_attachment :list_items, :image_main
-  end
-end
