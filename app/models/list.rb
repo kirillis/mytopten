326cb5364208308c3b11d1000a84e0af64215678
Kris Immig <kris@factorial.io>
2017-12-29 21:32:35 +0100

    Remove description requirement.

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,7 +4,6 @@ class List < ActiveRecord::Base
   has_many :list_items, dependent: :delete_all
   belongs_to :user
   validates :title, presence: true
-  validates :description, presence: true
 
   scope :published, -> { where(public: true) }
 
