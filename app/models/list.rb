494f3be23a18942bf733df13e546d32af8d156ea
BOXY\Kris Immig <kris.immig@gmail.com>
2018-01-06 16:03:42 +0100

    Add minimum items scope to lists.

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,8 +6,9 @@ class List < ActiveRecord::Base
   validates :title, presence: true
 
   scope :published, -> { where(public: true) }
+  scope :min_items, -> { where("list_items_count >= 3") }
 
   def self.search(search)
-    published.where("LOWER(title) LIKE '%#{search}%' OR LOWER(description) LIKE '%#{search}%'")
+    published.min_items.where("LOWER(title) LIKE '%#{search}%' OR LOWER(description) LIKE '%#{search}%'")
   end
 end
